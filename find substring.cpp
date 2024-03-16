e7b9bb5c51d4b5de7c56b5cc6a1640ef46137417
Mehedi Hasan Emon <154056997+mehediEmon004@users.noreply.github.com>
2024-03-16 14:32:46 +0600

    Update find substring.cpp

--- find substring.cpp
+++ find substring.cpp
@@ -1,7 +1,7 @@
 int find(string str, string substr){
     int strlen=str.size();
     int sublen=substr.size();
-    for(int i=0; i<strlen-sublen; i++){
+    for(int i=0; i<strlen-sublen+1; i++){
         int k=i,j;
         for(j=0; j<sublen; j++){
             if(substr[j]!=str[k]) break;
